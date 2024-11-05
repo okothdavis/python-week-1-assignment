4e39697a92661da6ac65763d8b832cc229904221
okothdavis <davisokoth15@gmail.com>
2024-11-05 18:19:35 +0300

    assignment complete

---
+++ user_input.py
@@ -0,0 +1,11 @@
+# Ask for the user's name
+name = input("Okoth Davis")
+
+# Ask for the user's age
+age = input("23")
+
+# Ask for the user's location
+location = input("Nairobi")
+
+# Print a personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
